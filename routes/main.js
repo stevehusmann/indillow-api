163b0de82c1ba35211693bb094fe669e573bcc8e
Steve Husmann <86138241+stevehusmann@users.noreply.github.com>
2022-03-24 23:02:20 -0500

    back to the start

--- routes/main.js
+++ routes/main.js
@@ -6,20 +6,20 @@ const axios = require('axios');
 
 const {Client} = require("@googlemaps/google-maps-services-js");
 
-const allowCors = fn => async (req, res) => {
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
-  if (req.method === 'OPTIONS') {
-    res.status(200).end()
-    return
-  }
-  return await fn(req, res)
-}
+// const allowCors = fn => async (req, res) => {
+//   res.setHeader('Access-Control-Allow-Credentials', true)
+//   res.setHeader('Access-Control-Allow-Origin', '*')
+//   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+//   res.setHeader(
+//     'Access-Control-Allow-Headers',
+//     'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+//   )
+//   if (req.method === 'OPTIONS') {
+//     res.status(200).end()
+//     return
+//   }
+//   return await fn(req, res)
+// }
 
 
 router.post("/jobs", async (req, res, next) => {
@@ -149,4 +149,4 @@ router.post("/jobdetails", async (req, res, next) => {
   });
 });
 
-module.exports = allowCors(router);
+module.exports = router;
