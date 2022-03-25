74f6d52e00ebd94ff89753cd3bc41b88fe783c02
Steve Husmann <86138241+stevehusmann@users.noreply.github.com>
2022-03-24 23:26:20 -0500

    changed port

--- routes/main.js
+++ routes/main.js
@@ -7,13 +7,7 @@ const axios = require('axios');
 const {Client} = require("@googlemaps/google-maps-services-js");
 
 router.post("/jobs", async (req, res, next) => {
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
+
   if (req.method === 'OPTIONS') {
     res.status(200).json({
       body: "OK"
