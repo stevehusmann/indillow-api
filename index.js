5e6abfd8bc8764680c92d1c4d7b3e4b4a3fdbb05
Steve Husmann <86138241+stevehusmann@users.noreply.github.com>
2022-03-24 20:40:13 -0500

    deploy back end

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+const mainRoutes = require("./routes/main");
+
+app.use(mainRoutes);
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
+
