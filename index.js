a68deb63a25bb7cf11351cf2804898b87b8cbe01
Stephen Husmann <stephenhusmann@Stephens-MBP-3.home>
2022-03-25 21:33:59 -0500

    more CORS

--- index.js
+++ index.js
@@ -12,15 +12,15 @@ app.use(
 
 app.use(cors());
 
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// });
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 const mainRoutes = require("./routes/main");
 
