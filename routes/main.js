f27fee9c43e641548b74adc8cb3c5e2fdb2536c7
Steve Husmann <86138241+stevehusmann@users.noreply.github.com>
2022-04-03 22:57:44 -0500

    refactor stuff

--- routes/main.js
+++ routes/main.js
@@ -24,11 +24,14 @@ async function getBrowserInstance() {
 	}
 
 	return chromium.puppeteer.launch({
-    args: [...chromium.args, "--hide-scrollbars", "--disable-web-security"],
-    defaultViewport: chromium.defaultViewport,
-    executablePath: await chromium.executablePath,
-    headless: true,
-    ignoreHTTPSErrors: true,
+		args: chromium.args,
+		defaultViewport: {
+			width: 1280,
+			height: 720
+		},
+		executablePath,
+		headless: chromium.headless,
+		ignoreHTTPSErrors: true
 	})
 }
 
@@ -39,54 +42,54 @@ router.post("/jobs", async (req, res, next) => {
   let browser = null;
   try {
     browser = await getBrowserInstance();
-    const page = await browser.newPage();
+    let page = await browser.newPage();
     await page.goto(URL, {waitUntil: "domcontentloaded"});
     const resultsArray = await page.evaluate(() => {
       try {
         return window.mosaic.providerData["mosaic-provider-jobcards"].metaData.mosaicProviderJobCardsModel.results;
       } catch (error) {
         return ('Puppeteer JobCard Error: ' + error);
-      } finally {
-          if (resultsArray) {
-            resultsArray.map(async (job) => {
-              if(!jobKeys.includes(job.jobkey)){
-                if(job.loceTagValueList) {
-                  let address = null;
-                  let neighborhood = null;
-                  job.loceTagValueList.map(locString => {
-                    const locationKey = locString.split('"')[1];
-                    const locationValue = locString.split('"')[3];
+      }
+    });
+      if (resultsArray) {
+        resultsArray.map(async (job) => {
+          if(!jobKeys.includes(job.jobkey)){
+            if(job.loceTagValueList) {
+              let address = null;
+              let neighborhood = null;
+              job.loceTagValueList.map(locString => {
+                const locationKey = locString.split('"')[1];
+                const locationValue = locString.split('"')[3];
 
-                    if (locationKey === 'address') {
-                      address = `${locationValue}, ${job.formattedLocation}`;
-                    } else if (locationKey === 'neighborhood') {
-                      neighborhood = locationValue;
-                    }
-                  });
-                  if (address) {
-                    jobsArray.push({
-                      key: job.jobkey,
-                      jobTitle: job.title,
-                      company: job.company,
-                      link: 'https://indeed.com' + job.link,
-                      urgentlyHiring: job.urgentlyHiring,
-                      salary: job.salarySnippet.text,
-                      address: address,
-                      neighborhood: neighborhood,
-                      jobTypes: job.jobTypes,
-                      logo: job.companyBrandingAttributes ? job.companyBrandingAttributes.logoUrl : null,              
-                      headerImageUrl: job.companyBrandingAttributes ? job.companyBrandingAttributes.headerImageUrl : null,
-                      formattedRelativeTime: job.formattedRelativeTime,
-
-                    });
-                  }
+                if (locationKey === 'address') {
+                  address = `${locationValue}, ${job.formattedLocation}`;
+                } else if (locationKey === 'neighborhood') {
+                  neighborhood = locationValue;
                 }
-                jobKeys.push(job.jobkey);
+              });
+              if (address) {
+                jobsArray.push({
+                  key: job.jobkey,
+                  jobTitle: job.title,
+                  company: job.company,
+                  link: 'https://indeed.com' + job.link,
+                  urgentlyHiring: job.urgentlyHiring,
+                  salary: job.salarySnippet.text,
+                  address: address,
+                  neighborhood: neighborhood,
+                  jobTypes: job.jobTypes,
+                  logo: job.companyBrandingAttributes ? job.companyBrandingAttributes.logoUrl : null,              
+                  headerImageUrl: job.companyBrandingAttributes ? job.companyBrandingAttributes.headerImageUrl : null,
+                  formattedRelativeTime: job.formattedRelativeTime,
+
+                });
               }
-            });
+            }
+            jobKeys.push(job.jobkey);
           }
-        }
-      });
+        });
+      }
+
 
     // add GeoLocation Data
     try {
