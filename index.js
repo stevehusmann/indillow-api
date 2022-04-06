f103ff5055e8c556041d970fcc8dc7646ba52e80
Steve Husmann <86138241+stevehusmann@users.noreply.github.com>
2022-04-06 16:58:33 -0500

    get rid of app.use resheaders

--- index.js
+++ index.js
@@ -10,15 +10,15 @@ app.use(
   })
 );
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
 
 app.use(cors());
 const mainRoutes = require("./routes/main");
