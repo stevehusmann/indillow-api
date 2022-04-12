00452e64c4b0a42b17ff494ce89a65d688f05d8b
Steve Husmann <86138241+stevehusmann@users.noreply.github.com>
2022-04-11 22:59:43 -0500

    console log

--- routes/main.js
+++ routes/main.js
@@ -113,8 +113,12 @@ router.get("/test", (req, res, next) => {
 
 router.post("/jobdetails", async (req, res, next) => {
   const URL = req.body.URL;
-  const $ = await fetchHTML(URL);
-  const jobDescription = $("#jobDescriptionText").html();
+  try {
+    const $ = await fetchHTML(URL);
+    const jobDescription = $("#jobDescriptionText").html();
+  } catch (e) {
+    console.log(e);
+  }
   
   res.send({
     jobDescription: jobDescription
