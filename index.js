a4e2c07a86e96896a115bbd2767af223b999044f
Steve Husmann <86138241+stevehusmann@users.noreply.github.com>
2022-04-11 21:48:59 -0500

    redploy to heroku

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.use(
   })
 );
 
+
 // app.use((req, res, next) => {
 //   res.header("Access-Control-Allow-Origin", "*");
 //   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
