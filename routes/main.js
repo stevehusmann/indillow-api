ca64b90e23e6efdbe78a40ecd9dbae85386c34df
Steve Husmann <86138241+stevehusmann@users.noreply.github.com>
2022-04-13 15:39:46 -0500

    fix progress bar

--- routes/main.js
+++ routes/main.js
@@ -1,13 +1,56 @@
 const dotenv = require('dotenv').config();
 const router = require("express").Router();
 const cheerio = require('cheerio');
+const request = require('request');
 const axios = require('axios');
 
 const {Client} = require("@googlemaps/google-maps-services-js");
+const res = require('express/lib/response');
+
+// async function proxyGenerator () {
+//   let ip_addresses = [];
+//   let port_numbers = [];
+//   const {data} = await axios.get("https://sslproxies.org/") 
+//   const $ = cheerio.load(data);
+
+//   $("td:nth-child(1)").each(function(index, value) {
+//     ip_addresses[index] = $(this).text();
+//   });
+
+//   $("td:nth-child(2)").each(function(index, value) {
+//     port_numbers[index] = $(this).text();
+//   });
+
+//   let random_number = Math.floor(Math.random() * 100);
+//   const proxy = {
+//     host: ip_addresses[random_number],
+//     port: port_numbers[random_number]
+//   }
+//   return proxy;
+// };
+  
+
+
+// async function fetchHTML(url) {
+//   const options = {
+//     url: url,
+//     method: "GET",
+//     proxy: await proxyGenerator()
+//   };
+//   console.log(options);
+  // request(options, function(error, response, html) {
+  //   if (!error && response.statusCode == 200) {
+  //     return cheerio.load(html);
+  //   } else {
+  //     console.log("Error scraping site, please try again");
+  //   }
+  // });
+
 
 async function fetchHTML(url) {
+  // const proxy = await proxyGenerator();
   try {
-    const { data } = await axios.get(url);
+    const { data } = await axios.get(url)
     return cheerio.load(data);
   }
   catch (error) {
@@ -15,13 +58,20 @@ async function fetchHTML(url) {
   }
 }
 
+router.post("/test", async (req, res, next) => {
+  const URL = req.body.URL;
+  const $ = await fetchHTML(URL);
+  res.send ($("#mosaic-data").html());
+});
+
+
 router.post("/jobs", async (req, res, next) => {
   const URL = req.body.URL;
   const jobKeys = req.body.jobKeys || [];
   const jobsArray = [];
     const $ = await fetchHTML(URL);
+    if ($) {
     const mosaicData = $("#mosaic-data").html();
-    if (mosaicData) {
     const firstTrim = mosaicData.split('window.mosaic.providerData["mosaic-provider-jobcards"]={"metaData":{"mosaicProviderJobCardsModel":')[1];
     const secondTrim = firstTrim.split(',"searchTimestamp"')[0];
     const resultsArrayString = secondTrim.split('"results":')[1];
@@ -88,7 +138,9 @@ router.post("/jobs", async (req, res, next) => {
         if ($('a[aria-label="Next"]')) {
           const href = $('a[aria-label="Next"]').attr('href');
           const pp = $('a[aria-label="Next"]').attr('data-pp');
-          nextURL = 'http://indeed.com' + href + '&pp=' + pp;
+          if (href && pp) {
+            nextURL = 'http://indeed.com' + href + '&pp=' + pp;
+          }
         } 
       } catch (e) {
         console.log('next button error: ' + e);
@@ -100,7 +152,7 @@ router.post("/jobs", async (req, res, next) => {
         jobKeys: jobKeys
       });
     } else {
-      console.log($.html());
+      console.log("cheerio error");
     }
   });
 
