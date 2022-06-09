526a1d744216d40d6090395a2953c5dac243e947
Steve Husmann <86138241+stevehusmann@users.noreply.github.com>
2022-06-09 12:16:11 -0500

    comment out IP address stuff

--- routes/main.js
+++ routes/main.js
@@ -29,12 +29,12 @@ async function fetchHTML(url) {
 
 
 router.post("/jobs", async (req, res, next) => {
-  const parseIp = (req) => req.headers['x-forwarded-for']?.split(',').shift() || req.socket?.remoteAddress;
-  const IP = parseIp(req);
+  // const parseIp = (req) => req.headers['x-forwarded-for']?.split(',').shift() || req.socket?.remoteAddress;
+  // const IP = parseIp(req);
   const URL = req.body.URL;
   const jobKeys = req.body.jobKeys || [];
   const jobsArray = [];
-    const $ = await fetchHTML(IP,URL);
+    const $ = await fetchHTML(URL);
     const mosaicData = $("#mosaic-data").html();
     if (mosaicData) {
     const firstTrim = mosaicData.split('window.mosaic.providerData["mosaic-provider-jobcards"]={"metaData":{"mosaicProviderJobCardsModel":')[1];
