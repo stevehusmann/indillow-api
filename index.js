598673ede5bd330c4f2005cc3c62c6651bae5639
Steve Husmann <86138241+stevehusmann@users.noreply.github.com>
2022-06-22 00:42:23 -0500

    trying puppeteer because something changed on indeed

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
 var cors = require('cors');
 const app = express();
 
@@ -9,11 +11,32 @@ app.use(
     extended: true,
   })
 );
-
+app.use(passport.initialize());
 app.use(cors());
 const mainRoutes = require("./routes/main");
 app.use(mainRoutes);
 
-const PORT = process.env.PORT || 80;
-app.listen(PORT, () => console.log(`CORS-enabled web server is running in port ${PORT}`));
+app.post(
+  "/login",
+  passport.authenticate("login", {
+    successRedirect: "/success",
+    failureRedirect: "/login",
+    session: false,
+  })
+);
 
+const PORT = process.env.PORT || 80;
+app.listen(PORT, () => console.log(`Web server is running in port ${PORT}`));
+
+passport.use(
+  "login",
+  new LocalStrategy((username, password, done) => {
+    const authenticated = username === "John" && password === "Smith";
+
+    if (authenticated) {
+      return done(null, { myUser: "user", myID: 1234 });
+    } else {
+      return done(null, false);
+    }
+  })
+);
--- routes/main.js
+++ routes/main.js
@@ -1,6 +1,7 @@
 const dotenv = require('dotenv').config();
 const router = require("express").Router();
 const cheerio = require('cheerio');
+const puppeteer = require('puppeteer');
 const httpsProxyAgent = require('https-proxy-agent');
 const {Client} = require("@googlemaps/google-maps-services-js");
 const res = require('express/lib/response');
@@ -24,91 +25,97 @@ router.post("/jobs", async (req, res, next) => {
   const URL = req.body.URL;
   const jobKeys = req.body.jobKeys || [];
   const jobsArray = [];
-    const $ = await fetchHTML(URL);
-    const mosaicData = $("#mosaic-data").html();
-    if (mosaicData) {
-    const firstTrim = mosaicData.split('window.mosaic.providerData["mosaic-provider-jobcards"]={"metaData":{"mosaicProviderJobCardsModel":')[1];
-    const secondTrim = firstTrim.split(',"searchTimestamp"')[0];
-    const resultsArrayString = secondTrim.split('"results":')[1];
-    const resultsArray = JSON.parse(resultsArrayString);
-    
-    if (Array.isArray(resultsArray)) {
-      resultsArray.map(async (job) => {
-        if(!jobKeys.includes(job.jobkey)){
-          if(job.loceTagValueList) {
-            let address = null;
-            let neighborhood = null;
-            job.loceTagValueList.map(locString => {
-              const locationKey = locString.split('"')[1];
-              const locationValue = locString.split('"')[3];
-              if (locationKey === 'address') {
-                address = `${locationValue}, ${job.formattedLocation}`;
-              } else if (locationKey === 'neighborhood') {
-                neighborhood = locationValue;
-              }
-            });
-            if (address) {
-              jobsArray.push({
-                key: job.jobkey,
-                jobTitle: job.title,
-                company: job.company,
-                link: 'https://indeed.com' + job.link,
-                urgentlyHiring: job.urgentlyHiring,
-                salary: job.salarySnippet.text,
-                address: address,
-                neighborhood: neighborhood,
-                jobTypes: job.jobTypes,
-                logo: job.companyBrandingAttributes ? job.companyBrandingAttributes.logoUrl : null,              
-                headerImageUrl: job.companyBrandingAttributes ? job.companyBrandingAttributes.headerImageUrl : null,
-                formattedRelativeTime: job.formattedRelativeTime,
-              });
-            }
-          }
-          jobKeys.push(job.jobkey);
-        }
-      });
-    }
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('http://api.scraperapi.com?api_key='+ process.env.SCRAPER_API_KEY + '&url=' + URL);
 
-      // add GeoLocation Data
-      try {
-        const addLocationData = await Promise.all(jobsArray.map(async (job) => {
-          const args = {
-            params: {
-              key: process.env.GOOGLE_MAPS_API_KEY,
-              address: job.address,
-            }
-          };
-          const client = new Client();
-          const geocode = await client.geocode(args).then(gcResponse => {
-            return gcResponse.data.results[0];
-          });
-          job.location = geocode.geometry.location;
-          job.placeId = geocode.place_id;
-        }))
-      } catch (error){
-        console.log("Geocode error: " + error)
-      }
-      let nextURL = null;
-      try {
-        if ($('a[aria-label="Next"]')) {
-          const href = $('a[aria-label="Next"]').attr('href');
-          const pp = $('a[aria-label="Next"]').attr('data-pp');
-          if (href && pp) {
-            nextURL = 'http://indeed.com' + href + '&pp=' + pp;
-          }
-        } 
-      } catch (e) {
-        console.log('next button error: ' + e);
-      }
-      console.log("Successfully scraped: " + URL);
-      res.send({
-        jobsArray: jobsArray,
-        nextURL: nextURL,
-        jobKeys: jobKeys
-      });
-    } else {
-      console.log($.html());
+  const resultsArray = await page.evaluate(() => {
+    try {
+      return window.mosaic.providerData["mosaic-provider-jobcards"].metaData.mosaicProviderJobCardsModel.results;
+    } catch (error) {
+      console.log('Puppeteer JobCard Error: ' + error);
     }
+  })
+  
+  if (Array.isArray(resultsArray)) {
+    resultsArray.map(async (job) => {
+      if(!jobKeys.includes(job.jobkey)){
+        if(job.loceTagValueList) {
+          let address = null;
+          let neighborhood = null;
+          job.loceTagValueList.map(locString => {
+            const locationKey = locString.split('"')[1];
+            const locationValue = locString.split('"')[3];
+            if (locationKey === 'address') {
+              address = `${locationValue}, ${job.formattedLocation}`;
+            } else if (locationKey === 'neighborhood') {
+              neighborhood = locationValue;
+            }
+          });
+          if (address) {
+            jobsArray.push({
+              key: job.jobkey,
+              jobTitle: job.title,
+              company: job.company,
+              link: 'https://indeed.com' + job.link,
+              urgentlyHiring: job.urgentlyHiring,
+              salary: job.salarySnippet.text,
+              address: address,
+              neighborhood: neighborhood,
+              jobTypes: job.jobTypes,
+              logo: job.companyBrandingAttributes ? job.companyBrandingAttributes.logoUrl : null,              
+              headerImageUrl: job.companyBrandingAttributes ? job.companyBrandingAttributes.headerImageUrl : null,
+              formattedRelativeTime: job.formattedRelativeTime,
+            });
+          }
+        }
+        jobKeys.push(job.jobkey);
+      }
+    });
+
+    // add GeoLocation Data
+    try {
+      const addLocationData = await Promise.all(jobsArray.map(async (job) => {
+        const args = {
+          params: {
+            key: process.env.GOOGLE_MAPS_API_KEY,
+            address: job.address,
+          }
+        };
+        const client = new Client();
+        const geocode = await client.geocode(args).then(gcResponse => {
+          return gcResponse.data.results[0];
+        });
+        job.location = geocode.geometry.location;
+        job.placeId = geocode.place_id;
+      }))
+    } catch (error){
+      console.log("Geocode error: " + error)
+    }
+    const nextURL = await page.evaluate(() => {
+      try {
+        const isNextButton = document.querySelector('a[aria-label="Next"]');
+        if(isNextButton) {
+          const href = document.querySelector('a[aria-label="Next"]').getAttribute('href');
+          const pp = document.querySelector('a[aria-label="Next"]').getAttribute('data-pp');
+          return 'http://indeed.com' + href + '&pp=' + pp;
+        } else {
+          return null;
+        }    
+      } catch (error) {
+        console.log('Puppeteer Next Button Error: ' + error);
+      }
+      
+    });
+    console.log("Successfully scraped: " + URL);
+    res.send({
+      jobsArray: jobsArray,
+      nextURL: nextURL,
+      jobKeys: jobKeys
+    });
+  } else {
+    console.log($.html());
+  }
   });
 
 
